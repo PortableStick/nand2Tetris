f0554e44e42e58c3c0d51dd8570be919f6f59ab9
PortableStick <crashingwaves@fea.st>
2017-06-15 16:30:38 -0600

    Fill responds to key press

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -1,14 +1,74 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+    //setup outer loop
+    @256
+    D=A
+    @maxRows
+    M=D
+    @rows
+    M=0
+    @currentRow
+    M=0
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+    (CHECK)
+    @KBD
+    D=M
+    @OUTER
+    D;JNE
+    @CHECK
+    0;JMP
 
-// Put your code here.
+    (OUTER)
+    //check rows
+    @rows
+    D=M
+    @maxRows
+    D=D-M
+    //go to @END if @rows ≥ 0
+    @CHECK
+    D;JGE
+    //inner loop
+    //setup inner loop
+    @32
+    D=A
+    @maxCols
+    M=D
+    @cols
+    M=0
+
+    (INNER)
+    //check cols
+    @cols
+    D=M
+    @maxCols
+    D=D-M
+    //go to @SLOC if @cols ≥ 0
+    @SLOC
+    D;JGE
+    //set screen pointer
+    @currentRow
+    D=M
+    @cols
+    D=D+M
+    @SCREEN
+    D=D+A
+    A=D
+    //fill pixels
+    M=-1
+    //increment
+    @cols
+    M=M+1
+    @INNER
+    0;JMP
+
+    (SLOC)
+    @32
+    D=A
+    @currentRow
+    M=M+D
+    @rows
+    M=M+1
+    @OUTER
+    0;JMP
+
+    (END)
+    @END
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,38 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @R2
+    M=0
+    @R0
+    D=M
+    @n
+    M=D
+    @i
+    M=0
+    (LOOP)
+    //jump to end if @i is zero
+    @i
+    D=M
+    @n
+    D=D-M
+    @END
+    D;JGE
+
+    //add R1 to total
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    //increment @i
+    @i
+    M=M+1
+
+    //jump to @LOOP
+    @LOOP
+    0;JMP
+
+    (END)
+    //infinite loop
+    @END
+    0;JMP
